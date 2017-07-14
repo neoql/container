fc01a603ba0fafb875332ff8c9217f67af033352
newql-moons <1156325827@qq.com>
2017-07-14 20:13:04 +0800

    修改了单元测试

--- treemap/treemap_test.go
+++ treemap/treemap_test.go
@@ -8,12 +8,23 @@ func TestPutAndGet(t *testing.T) {
 	tm := New()
 	pre, err := tm.Put("Hello", "World")
 
-	t.Log(pre)
-	t.Log(err)
+	if pre != nil {
+		t.Error("pre shuould be nil.")
+	}
 
 	value, err := tm.Get("Hello")
-	t.Log(err)
 	if value != "World" || err != nil {
+		t.Error(value)
+	}
+
+	pre, err = tm.Put("Hello", "TreeMap")
+	if pre != "World" {
+		t.Error(pre)
+	}
+
+	tm.Put("Hi", "World")
+	value, err = tm.Get("Hi")
+	if value != "World" {
 		t.Error(value)
 	}
 }
